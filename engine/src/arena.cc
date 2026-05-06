356ea7f0fe8b6e13c8b99dcceb9f8d300444059e
namnkahn1607 <nlnkandthebois@gmail.com>
2026-05-06 10:53:35 +0700

    Wrap GC in try-catch to handle throwable write-shredding

--- engine/src/arena.cc
+++ engine/src/arena.cc
@@ -42,111 +42,119 @@ MemoryArena::~MemoryArena() {
 
 void MemoryArena::RunGarbageCollector(
     const std::atomic<bool>& g_shutdown_request) {
-    while (!g_shutdown_request.load(std::memory_order_relaxed)) {
-        const uint64_t head = write_head.load(std::memory_order_relaxed);
-        const uint64_t tail = read_tail.load(std::memory_order_relaxed);
+    try {
+        while (!g_shutdown_request.load(std::memory_order_relaxed)) {
+            const uint64_t head = write_head.load(std::memory_order_relaxed);
+            const uint64_t tail = read_tail.load(std::memory_order_relaxed);
 
-        if (const uint64_t used_space = head - tail;
-            used_space < engine::LOW_WATERMARK_THRESHOLD) {
-            std::this_thread::sleep_for(
-                std::chrono::milliseconds(engine::LOW_GC_SLEEP_MS));
-            continue;
-        } else if (used_space < engine::HIGH_WATERMARK_THRESHOLD) {
-            std::this_thread::sleep_for(
-                std::chrono::milliseconds(engine::HIGH_GC_SLEEP_MS));
-        }
-
-        /* The Snowplow mechanism */
-        const uint64_t tail_index = tail & (engine::PAYLOAD_BUFFER_SIZE - 1);
-
-        // Perform leaping in case approaching buffer border
-        if (const uint64_t padding = engine::PAYLOAD_BUFFER_SIZE - tail_index;
-            padding < sizeof(PayloadHeader)) {
-            read_tail.fetch_add(padding, std::memory_order_relaxed);
-            continue;
-        }
-
-        // Read payload header
-        const auto* header =
-            reinterpret_cast<PayloadHeader*>(buffer_payload + tail_index);
-
-        // Approach an invalid header identifier, advance tail by 1.
-        if (header->identifier != engine::VALID_IDENTIFIER) {
-            read_tail.fetch_add(1, std::memory_order_relaxed);
-            continue;
-        }
-
-        const uint32_t node_id = header->node_id;
-        const uint32_t text_len = header->length;
-        const uint32_t total_size = sizeof(PayloadHeader) + text_len;
-        MetaNode& node = metadata[node_id];
-        auto [state, ref_bit, length, v_offset] = node.LoadControl();
-
-        if (state == NodeState::DEAD ||
-            v_offset != (tail & engine::VIRTUAL_OFFSET_MASK)) {
-            read_tail.fetch_add(total_size, std::memory_order_relaxed);
-            continue;
-        }
-
-        if (ref_bit == EvictState::COLD) {
-            // Evict in case encountering cold node
-            uint64_t expected_ctrl =
-                PackControl(state, ref_bit, length, v_offset);
-            const uint64_t desired_ctrl =
-                PackControl(NodeState::DEAD, ref_bit, length, v_offset);
-
-            node.control_block.compare_exchange_strong(
-                expected_ctrl, desired_ctrl, std::memory_order_release,
-                std::memory_order_relaxed);
-
-            node.created_at.store(0, std::memory_order_relaxed);
-        } else {
-            // Perform payload rescue & give it a Second chance
-            // if the node is still hot.
-            const uint64_t rescued_offset = AllocatePayload(text_len);
-            const uint64_t rescued_index =
-                rescued_offset & (engine::PAYLOAD_BUFFER_SIZE - 1);
-
-            PayloadHeader new_header{engine::VALID_IDENTIFIER, node_id,
-                                     text_len};
-            std::memcpy(buffer_payload + rescued_index, &new_header,
-                        sizeof(PayloadHeader));
-
-            uint64_t src_idx = (tail + sizeof(PayloadHeader)) &
-                               (engine::PAYLOAD_BUFFER_SIZE - 1);
-            uint64_t dst_idx = (rescued_offset + sizeof(PayloadHeader)) &
-                               (engine::PAYLOAD_BUFFER_SIZE - 1);
-            uint64_t bytes_left = text_len;
-
-            while (bytes_left > 0) {
-                uint64_t src_continuous = engine::PAYLOAD_BUFFER_SIZE - src_idx;
-                uint64_t dst_continuous = engine::PAYLOAD_BUFFER_SIZE - dst_idx;
-
-                const uint64_t chunk_size =
-                    std::min({bytes_left, src_continuous, dst_continuous});
-
-                std::memcpy(buffer_payload + dst_idx, buffer_payload + src_idx,
-                            chunk_size);
-
-                bytes_left -= chunk_size;
-                src_idx =
-                    (src_idx + chunk_size) & (engine::PAYLOAD_BUFFER_SIZE - 1);
-                dst_idx =
-                    (dst_idx + chunk_size) & (engine::PAYLOAD_BUFFER_SIZE - 1);
+            if (const uint64_t used_space = head - tail;
+                used_space < engine::LOW_WATERMARK_THRESHOLD) {
+                std::this_thread::sleep_for(
+                    std::chrono::milliseconds(engine::LOW_GC_SLEEP_MS));
+                continue;
+            } else if (used_space < engine::HIGH_WATERMARK_THRESHOLD) {
+                std::this_thread::sleep_for(
+                    std::chrono::milliseconds(engine::HIGH_GC_SLEEP_MS));
             }
 
-            uint64_t expected_ctrl =
-                PackControl(state, EvictState::HOT, length, v_offset);
-            const uint64_t desired_ctrl =
-                PackControl(state, EvictState::COLD, length, rescued_offset);
+            /* The Snowplow mechanism */
+            const uint64_t tail_index =
+                tail & (engine::PAYLOAD_BUFFER_SIZE - 1);
 
-            node.control_block.compare_exchange_strong(
-                expected_ctrl, desired_ctrl, std::memory_order_release,
-                std::memory_order_relaxed);
+            // Perform leaping in case approaching buffer border
+            if (const uint64_t padding =
+                    engine::PAYLOAD_BUFFER_SIZE - tail_index;
+                padding < sizeof(PayloadHeader)) {
+                read_tail.fetch_add(padding, std::memory_order_relaxed);
+                continue;
+            }
+
+            // Read payload header
+            const auto* header =
+                reinterpret_cast<PayloadHeader*>(buffer_payload + tail_index);
+
+            // Approach an invalid header identifier, advance tail by 1.
+            if (header->identifier != engine::VALID_IDENTIFIER) {
+                read_tail.fetch_add(1, std::memory_order_relaxed);
+                continue;
+            }
+
+            const uint32_t node_id = header->node_id;
+            const uint32_t text_len = header->length;
+            const uint32_t total_size = sizeof(PayloadHeader) + text_len;
+            MetaNode& node = metadata[node_id];
+            auto [state, ref_bit, length, v_offset] = node.LoadControl();
+
+            if (state == NodeState::DEAD ||
+                v_offset != (tail & engine::VIRTUAL_OFFSET_MASK)) {
+                read_tail.fetch_add(total_size, std::memory_order_relaxed);
+                continue;
+            }
+
+            if (ref_bit == EvictState::COLD) {
+                // Evict in case encountering cold node
+                uint64_t expected_ctrl =
+                    PackControl(state, ref_bit, length, v_offset);
+                const uint64_t desired_ctrl =
+                    PackControl(NodeState::DEAD, ref_bit, length, v_offset);
+
+                node.control_block.compare_exchange_strong(
+                    expected_ctrl, desired_ctrl, std::memory_order_release,
+                    std::memory_order_relaxed);
+
+                node.created_at.store(0, std::memory_order_relaxed);
+            } else {
+                // Perform payload rescue & give it a Second chance
+                // if the node is still hot.
+                const uint64_t rescued_offset = AllocatePayload(text_len);
+                const uint64_t rescued_index =
+                    rescued_offset & (engine::PAYLOAD_BUFFER_SIZE - 1);
+
+                PayloadHeader new_header{engine::VALID_IDENTIFIER, node_id,
+                                         text_len};
+                std::memcpy(buffer_payload + rescued_index, &new_header,
+                            sizeof(PayloadHeader));
+
+                uint64_t src_idx = (tail + sizeof(PayloadHeader)) &
+                                   (engine::PAYLOAD_BUFFER_SIZE - 1);
+                uint64_t dst_idx = (rescued_offset + sizeof(PayloadHeader)) &
+                                   (engine::PAYLOAD_BUFFER_SIZE - 1);
+                uint64_t bytes_left = text_len;
+
+                while (bytes_left > 0) {
+                    uint64_t src_continuous =
+                        engine::PAYLOAD_BUFFER_SIZE - src_idx;
+                    uint64_t dst_continuous =
+                        engine::PAYLOAD_BUFFER_SIZE - dst_idx;
+
+                    const uint64_t chunk_size =
+                        std::min({bytes_left, src_continuous, dst_continuous});
+
+                    std::memcpy(buffer_payload + dst_idx,
+                                buffer_payload + src_idx, chunk_size);
+
+                    bytes_left -= chunk_size;
+                    src_idx = (src_idx + chunk_size) &
+                              (engine::PAYLOAD_BUFFER_SIZE - 1);
+                    dst_idx = (dst_idx + chunk_size) &
+                              (engine::PAYLOAD_BUFFER_SIZE - 1);
+                }
+
+                uint64_t expected_ctrl =
+                    PackControl(state, EvictState::HOT, length, v_offset);
+                const uint64_t desired_ctrl = PackControl(
+                    state, EvictState::COLD, length, rescued_offset);
+
+                node.control_block.compare_exchange_strong(
+                    expected_ctrl, desired_ctrl, std::memory_order_release,
+                    std::memory_order_relaxed);
+            }
+
+            // Move read tail forward anyway
+            read_tail.fetch_add(total_size, std::memory_order_relaxed);
         }
-
-        // Move read tail forward anyway
-        read_tail.fetch_add(total_size, std::memory_order_relaxed);
+    } catch (const std::exception& e) {
+        std::cout << "[Engine] WARNING: " << e.what() << std::endl;
     }
 }
 
