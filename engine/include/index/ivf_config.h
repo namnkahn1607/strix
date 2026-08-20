e39526bb9870ad941ceb874e69cf3d2d8f653cd5
namnkahn1607 <nlnkandthebois@gmail.com>
2026-08-20 20:38:21 +0700

    feat: break several fields of IVF config into K-means config

--- engine/include/index/ivf_config.h
+++ engine/include/index/ivf_config.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-// IvfConfig defines the specification of IVF at construction time.
+// IvfConfig defines runtime specification of IVF at construction time.
 //
 // All validation are performed inside the IVF constructor.
 // NOTE: The average number of vectors per cluster: `max_slots / num_clusters`.
@@ -15,43 +15,15 @@ struct IvfConfig {
     // Must be greater than the average number of vectors per cluster.
     const uint32_t max_cluster_size;
 
-    // The sample space size K-means process decides to take on.
-    const uint32_t kmeans_sample_size;
-
     // Page prefault enabled? (default: `true`).
     const bool prefault = true;
 
-    // Number of successful Compactions since last episode published
-    // before starting a new one.
-    const uint32_t recalibration_trigger_count;
-
-    // Samples drawn per mini-batch step.
-    const uint32_t mini_batch_size;
-
-    // Number of mini-batch step per episode.
-    // Bounds worst-case episode duration regardless of `convergence_epsilon`
-    // is ever reached or not.
-    const uint32_t max_lloyd_iterations;
-
-    // Early termination threshold if total centroid movement dips below it.
-    const float convergence_epsilon;
-
-    // Minimum wait time after a publish before starting a new episode.
-    const uint64_t grace_period_seconds;
-
-    // Configures `1024` clusters, `1024` max size, `65'536` sample size
-    // with page prefault enabled.
-    static IvfConfig Production() {
-        return IvfConfig{
+    // Configures `1024` clusters, `1024` max size with page prefault.
+    static IvfConfig Standard() {
+        return {
             /*num_clusters=*/1'024,
             /*max_cluster_size=*/1'024,
-            /*kmeans_sample_size=*/65'536,
             /*prefault=*/true,
-            /*recalibration_trigger_count=*/16'384,
-            /*mini_batch_size=*/1024,
-            /*max_lloyd_iterations=*/16,
-            /*convergence_epsilon=*/1e-4f,
-            /*grace_period_seconds=*/1,
         };
     }
 };
